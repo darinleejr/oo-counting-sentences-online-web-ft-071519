475501c2c35da78d67eab0c5893920f6152d832a
Darin Webster <darinleenavy@gmail.com>
2019-09-12 20:19:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "HI.".end_with?(".")
+    "HI!".end_with?(".")
   end
 
   def question?
-
+    "What's your name?".end_with?("?")
+    "What's your name?".end_with?("!")
   end
 
   def exclamation?
-
+    "Hi, my name is Sophie!".end_with?("!")
+    "Hi, my name is Sophie.".end_with?("!")
   end
 
   def count_sentences
-
+    "First sentence. Second sentence. Is this the third sentence?".scan(/[^\?.!]+[\?!.]/).map(&:strip).count
+    "First sentence".scan(/[^.!?]+[\.?!]/).map(&:strip).count
+    "First senctence. Second sentence. Is this the third sentence? I like this sentence!".scan(/[^\.?!]+[\.?!]/).map(&:strip).count
   end
 end
